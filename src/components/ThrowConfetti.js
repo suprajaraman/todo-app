36cc28adcb1f5744f0b3dd5a2c24df0100a96b0e
supraja <suprajaraman2001@gmail.com>
2020-12-12 02:29:33 +0530

    added confetti and modal

---
+++ src/components/ThrowConfetti.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Confetti from 'react-confetti'
+import Modal from './Modal'
+
+const ThrowConfetti = ({ confetti }) => {
+      const { width, height } = window.screen
+      return (
+            <span className="confetti">
+                  {confetti && <>
+                        <Confetti
+                        width={width}
+                        height={height}
+                        numberOfPieces={2000}
+                        recycle={false}
+                        tweenDuration = {3000}
+                              />
+                              <Modal />
+                        </>}
+            </span>
+      )
+}
+
+export default ThrowConfetti
+
+
+// const confettiHandler = () => {
+//       s
+// }
+// onClick = {confettiHandler}
